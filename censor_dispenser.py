2b0cb5c459b02f2b3e18ccc85dc4ad7eca51d299
UJJY <ujj.aggarwal@gmail.com>
2020-05-11 15:42:56 +0400

    Finally finished 2 censor functions; word/phrase and list.

--- censor_dispenser.py
+++ censor_dispenser.py
@@ -4,4 +4,16 @@ email_two = open("email_two.txt", "r").read()
 email_three = open("email_three.txt", "r").read()
 email_four = open("email_four.txt", "r").read()
 
-penis poonis
+def censor_choice(text, censor_phrase):
+    text_censored = text.replace(censor_phrase, "⬛" * len(censor_phrase))
+    return text_censored
+
+#print(censor_choice(email_one, "learning algorithms"))
+
+proprietary_terms = ["she", "personality matrix", "sense of self", "self-preservation", "learning algorithm", "her", "herself"]
+def censor_list(text, lst):
+    for word in lst:
+        text = text.replace(word, "⬛" * len(word))
+    return(text)
+
+#print(censor_list(email_two, proprietary_terms))
